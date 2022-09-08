805adbfb7d2cfab57159e0cd116130422104d8d2
Rimon Ahmed <rimonians@gmail.com>
2022-09-08 12:26:08 +0600

    Project done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-console.log("Hello world!");
+const firstNum = document.querySelector("#firstNum");
+const secondNum = document.querySelector("#secondNum");
+const operator = document.querySelector("#operator");
+const calculate = document.querySelector("#calculate");
+const result = document.querySelector("#result");
+
+calculate.addEventListener("click", () => {
+  const firstNumVal = Number(firstNum.value);
+  const secondNumVal = Number(secondNum.value);
+  const operatorVal = operator.value;
+
+  if (operatorVal === "+") {
+    result.textContent = `Addition of ${firstNumVal} & ${secondNumVal} is : ${
+      firstNumVal + secondNumVal
+    }`;
+  } else if (operatorVal === "-") {
+    result.textContent = `Substraction of ${firstNumVal} & ${secondNumVal} is : ${
+      firstNumVal - secondNumVal
+    }`;
+  } else if (operatorVal === "*") {
+    result.textContent = `Multiplication of ${firstNumVal} & ${secondNumVal} is : ${
+      firstNumVal * secondNumVal
+    }`;
+  } else if (operatorVal === "/") {
+    result.textContent = `Division of ${firstNumVal} & ${secondNumVal} is : ${
+      firstNumVal / secondNumVal
+    }`;
+  }
+});
